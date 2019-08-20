f876be0b9bd6ee4dad00fd96e4fda732a3167f13
Bryan Pfremmer <pfremm@users.noreply.github.com>
2019-08-19 21:35:06 -0500

    Update build.sh
    Fixing bug in build.sh for second build-arg

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 
 s6_overlay_release=$(curl -s https://api.github.com/repos/just-containers/s6-overlay/releases/latest | jq .tag_name | tr -d \")
-docker build --build-arg ARCH=aarch64 S6_OVERLAY_RELEASE=$s6_overlay_release . 
+docker build --build-arg ARCH=aarch64 --build-arg S6_OVERLAY_RELEASE=$s6_overlay_release . 
